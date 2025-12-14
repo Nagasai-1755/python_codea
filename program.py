49c0d06a07b7ae0a53dcf534bd27dde8a8782195
naga sai <k.n.s.vamshikrishna@gmail.com>
2025-12-14 12:02:44 +0530

    added to even_odd

--- program.py
+++ program.py
@@ -1,1 +1,24 @@
-print('Hii Im nagasai')
+a=5
+if a%2==0:
+    print("Even")
+else:
+    print("Odd")
+
+print("Done") 
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+print("Done")   
+
+
+
+print("Finished Execution")
